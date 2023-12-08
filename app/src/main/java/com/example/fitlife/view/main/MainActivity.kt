22f4ad5edbfd8277458a5747f6d210cbcf118029
graceclaudia1719 <grcxlr@gmail.com>
2023-12-09 02:39:35 +0800

    add main layout

--- app/src/main/java/com/example/fitlife/view/main/MainActivity.kt
+++ app/src/main/java/com/example/fitlife/view/main/MainActivity.kt
@@ -29,9 +29,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val layoutManager = LinearLayoutManager(this)
-//        binding.rvStories.layoutManager = layoutManager
-
         viewModel.getSession().observe(this) { user ->
             if (!user.isLogin) {
                 startActivity(Intent(this, WelcomeActivity::class.java))
@@ -39,11 +36,10 @@ class MainActivity : AppCompatActivity() {
             } else {
                 token = user.token
             }
-//            setupAction(token)
+//            setupActin(token)
         }
 
         setupView()
-//        playAnimation()
     }
 
     private fun setupView() {
@@ -59,72 +55,16 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
     }
 
-//    private fun setupAction(token:String) {
-//        viewModel.getAllStories(token).observe(this) { result ->
-//            if (result != null) {
-//                when (result) {
-//                    is Result.Loading -> {
-//                        binding.progressBar.visibility = View.VISIBLE
-//                    }
-//
-//                    is Result.Success -> {
-//                        binding.progressBar.visibility = View.GONE
-//                        setStories(result.data.listStory as List<ListStoryItem>)
-//                        Toast.makeText(
-//                            this,
-//                            "success get stories",
-//                            Toast.LENGTH_SHORT
-//                        ).show()
-//                    }
-//
-//                    is Result.Error -> {
-//                        Toast.makeText(
-//                            this,
-//                            result.error,
-//                            Toast.LENGTH_SHORT
-//                        ).show()
-//
-//                    }
-//                }
-//            }
-//        }
+    private fun setupAction(token:String) {
 
 //        binding.addFab.setOnClickListener{
 //            val moveIntent = Intent(this@MainActivity, AddStoryActivity::class.java)
 //            startActivity(moveIntent)
 //        }
 
-//        binding.logoutButton.setOnClickListener {
-//            viewModel.logout()
-//        }
-//    }
-
-//    private fun setStories(stories: List<ListStoryItem>) {
-//        val storyAdapter = StoryAdapter(stories)
-//        storyAdapter.submitList(stories)
-//        binding.rvStories.adapter = storyAdapter
-//
-//        storyAdapter.setOnItemClickCallback(object : StoryAdapter.OnItemClickCallback {
-//            override fun onItemClicked(data: ListStoryItem) {
-//                if (data.name!=null && data.description!=null && data.photoUrl!=null){
-//                    val story : Story = Story(data.name, data.photoUrl, data.description)
-//                    val moveWithDataIntent = Intent(this@MainActivity, DetailActivity::class.java)
-//                    moveWithDataIntent.putExtra(DetailActivity.EXTRA_STORY, story)
-//                    startActivity(moveWithDataIntent)
-//                }
-//            }
-//        })
-//    }
-
-//    private fun playAnimation() {
-//
-//        val message = ObjectAnimator.ofFloat(binding.messageTextView, View.ALPHA, 1f).setDuration(400)
-//        val btnLogout = ObjectAnimator.ofFloat(binding.logoutButton, View.ALPHA, 1f).setDuration(400)
-//
-//        AnimatorSet().apply {
-//            playSequentially(message, btnLogout)
-//            start()
-//        }
-//    }
+        binding.logoutButton.setOnClickListener {
+            viewModel.logout()
+        }
+    }
 
 }
